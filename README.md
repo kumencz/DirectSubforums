e32a3735010de9310c436f18495d4e817112d8c2
kumencz <kubape12@gmail.com>
2017-05-15 15:07:32 +0200

    Initial working commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# Show only direct subforums
+
+## Installation
+
+Copy the extension to phpBB/ext/kumen/DirectSubforums
+
+Go to "ACP" > "Customise" > "Extensions" and enable the "Show only direct subforums" extension.
+
+## License
+
+[GPLv2](license.txt)
---
+++ event/main_listener.php
@@ -0,0 +1,50 @@
+<?php
+/**
+ *
+ * Show only direct subforums. An extension for the phpBB Forum Software package.
+ *
+ * @copyright (c) 2017, kumen, https://github.com/kumencz
+ * @license GNU General Public License, version 2 (GPL-2.0)
+ *
+ */
+
+namespace kumen\DirectSubforums\event;
+
+/**
+ * @ignore
+ */
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+
+/**
+ * Show only direct subforums Event listener.
+ */
+class main_listener implements EventSubscriberInterface
+{
+	static public function getSubscribedEvents()
+	{
+		return array(
+			'core.display_forums_modify_forum_rows'	=> 'drop_subforums',
+		);
+	}
+
+	/**
+	 * A sample PHP event
+	 * Modifies the names of the forums on index
+	 *
+	 * @param \phpbb\event\data	$event	Event object
+	 */
+	public function drop_subforums($event)
+	{
+		$row = $event['row'];
+		$parent_id = $event['parent_id'];
+		$forum_rows = $event['forum_rows'];
+		$subforums = $event['subforums'];
+		if($row['forum_type'] != FORUM_CAT && $parent_id != $row['parent_id'])
+		{
+			unset($forum_rows[$parent_id][$row['forum_id']]);
+			unset($subforums[$parent_id][$row['forum_id']]);
+		}
+		$event['forum_rows'] = $forum_rows;
+		$event['subforums'] = $subforums;
+	}
+}
